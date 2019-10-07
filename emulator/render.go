7c2d8fd4f18f3cb8e8b8ae11bdf6d676d1a0a2aa
akatsuki-py <bluejapan73+dev@gmail.com>
2019-10-07 16:50:16 +0900

    wip

--- emulator/render.go
+++ emulator/render.go
@@ -1,6 +1,8 @@
 package emulator
 
 import (
+	"sync"
+
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/pixelgl"
 )
@@ -8,6 +10,11 @@ import (
 const (
 	width  = 256
 	height = 240
+)
+
+var (
+	lineWait  sync.WaitGroup
+	lineMutex sync.Mutex
 )
 
 // Render 画面描画を行う
@@ -35,7 +42,7 @@ func (cpu *CPU) Render() {
 		var pixel2sprite map[uint16]([2]byte)
 		pixel2sprite = map[uint16]([2]byte){}
 		for i := 0; i < 64; i++ {
-			pixelX, pixelY := cpu.PPU.sRAM[i*4+3], (cpu.PPU.sRAM[i*4])
+			pixelX, pixelY := cpu.PPU.sRAM[i*4+3], (cpu.PPU.sRAM[i*4])+1
 			spriteNum := cpu.PPU.sRAM[i*4+1]
 			attr := cpu.PPU.sRAM[i*4+2]
 			pixel2sprite[(uint16(pixelY)<<8)|uint16(pixelX)] = [2]byte{spriteNum, attr}
@@ -44,35 +51,47 @@ func (cpu *CPU) Render() {
 		// BG・SPR描画
 		BGBatch.Clear()
 		SPRBatch.Clear()
-		for y := 0; y < height+1; y++ {
+		for y := 0; y < height; y++ {
 			cpu.RAM[0x2002] &= 0xbf
 			if y < height {
+				lineWait.Add(width)
 				for x := 0; x < width; x++ {
-					sprite, ok := pixel2sprite[(uint16(y)<<8)|uint16(x)]
-					if ok {
-						spriteNum, attr := sprite[0], sprite[1]
-						if spriteNum == 0 {
-							cpu.RAM[0x2002] |= 0x40
+					go func(x int) {
+						sprite, ok := pixel2sprite[(uint16(y)<<8)|uint16(x)]
+						if ok {
+							spriteNum, attr := sprite[0], sprite[1]
+							if spriteNum == 0 {
+								cpu.RAM[0x2002] |= 0x40
+							}
+							if attr&0x20 == 0 {
+								rect := cpu.PPU.outputSpriteRect(spriteNum, attr)
+								SPRSprite := pixel.NewSprite(cpu.PPU.SPRBuf, rect)
+								matrix := pixel.IM.Moved(pixel.V(float64(x), float64(height-y)))
+								lineMutex.Lock()
+								SPRSprite.Draw(SPRBatch, matrix)
+								lineMutex.Unlock()
+							}
 						}
-						if attr&0x20 == 0 {
-							rect := cpu.PPU.outputSpriteRect(spriteNum, attr)
-							SPRSprite := pixel.NewSprite(cpu.PPU.SPRBuf, rect)
-							matrix := pixel.IM.Moved(pixel.V(float64(x), float64(height-y)))
-							SPRSprite.Draw(SPRBatch, matrix)
-						}
-					}
 
-					rect := cpu.PPU.outputBGRect(uint(x), uint(y))
-					BGSprite := pixel.NewSprite(cpu.PPU.BGBuf, rect)
-					matrix := pixel.IM.Moved(pixel.V(float64(x), float64(height-y)))
-					BGSprite.Draw(BGBatch, matrix)
+						rect := cpu.PPU.outputBGRect(uint(x), uint(y))
+						BGSprite := pixel.NewSprite(cpu.PPU.BGBuf, rect)
+						matrix := pixel.IM.Moved(pixel.V(float64(x), float64(height-y)))
+
+						lineMutex.Lock()
+						BGSprite.Draw(BGBatch, matrix)
+						lineMutex.Unlock()
+
+						lineWait.Done()
+					}(x)
 				}
-			} else if y == height {
-				cpu.mutex.Lock()
-				cpu.setVBlank()
-				cpu.mutex.Unlock()
+				lineWait.Wait()
 			}
 		}
+
+		cpu.mutex.Lock()
+		cpu.setVBlank()
+		cpu.mutex.Unlock()
+
 		BGBatch.Draw(win)
 		SPRBatch.Draw(win)
 
