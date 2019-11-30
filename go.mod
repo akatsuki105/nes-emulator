2a925b8f02887d5e21bc3da59d4b76ef50a66a92
akatsuki-py <bluejapan73+dev@gmail.com>
2019-11-30 23:54:12 +0900

    use tfd for ROM select

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module nes-emulator
 go 1.13
 
 require (
+	github.com/akatsuki-py/tfd v0.0.1
 	github.com/faiface/pixel v0.8.1-0.20191105235048-e51d4a6676fa
 	golang.org/x/image v0.0.0-20190523035834-f03afa92d3ff
 )
--- nes.go
+++ nes.go
@@ -1,17 +1,23 @@
 package main
 
 import (
-	"flag"
+	"fmt"
 	"io/ioutil"
 	"path/filepath"
 
 	"nes-emulator/emulator"
+
+	"github.com/akatsuki-py/tfd"
 	"github.com/faiface/pixel/pixelgl"
 )
 
 func main() {
-	flag.Parse()
-	bytes := readNES(flag.Arg(0))
+	path, err := tfd.CreateSelectDialog([]string{"nes"}, false)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	bytes := readNES(path)
 
 	cpu := &emulator.CPU{}
 	cpu.LoadROM(bytes)
